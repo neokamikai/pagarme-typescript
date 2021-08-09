1571848f507f38c3684fd1ba56523326cb672883
Douglas Alves Domingues <>
2021-08-09 18:50:42 -0300

    add response interface **IAntifraudAnalysesGetDetailResponse**

---
+++ src/@types/interfaces/api/responses/namespaces/antifraud-analyses/getDetail/index.ts
@@ -0,0 +1,6 @@
+import IAntifraudAnalysesEntry from 'src/@types/interfaces/api/common/antifraud-analyses';
+
+export default interface IAntifraudAnalysesGetDetailResponse
+  extends IAntifraudAnalysesEntry{
+
+}
