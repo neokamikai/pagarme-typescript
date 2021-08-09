369d7fa0ca746721715c53593aef724edfb498b9
Douglas Alves Domingues <>
2021-08-09 18:46:18 -0300

    add common interface **IAntifraudAnalysesEntry**

---
+++ src/@types/interfaces/api/common/antifraud-analyses/index.ts
@@ -0,0 +1,10 @@
+export default interface IAntifraudAnalysesEntry {
+  object: 'antifraud_analysis',
+  name: string
+  score: string
+  cost: string
+  status: string
+  date_created: string
+  date_updated: string
+  id: string
+}
