b2fb9686b701089113efafccb115e0a2e6a3b0d8
Douglas Alves Domingues <>
2021-08-09 18:52:53 -0300

    add response interface **IAntifraudAnalysesGetListResponse**

---
+++ src/@types/interfaces/api/responses/namespaces/antifraud-analyses/getList/index.ts
@@ -0,0 +1,6 @@
+import IAntifraudAnalysesEntry from 'src/@types/interfaces/api/common/antifraud-analyses';
+
+export default interface IAntifraudAnalysesGetListResponse
+  extends Array<IAntifraudAnalysesEntry> {
+
+}
