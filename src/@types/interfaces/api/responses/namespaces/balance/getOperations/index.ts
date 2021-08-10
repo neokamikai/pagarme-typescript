9d798091f7923654ac313bdb41fd3730aae9ab55
Douglas Alves Domingues <>
2021-08-10 08:14:42 -0300

    add response interface **IBalanceGetOperationsResponse**

---
+++ src/@types/interfaces/api/responses/namespaces/balance/getOperations/index.ts
@@ -0,0 +1,5 @@
+import IBalanceOperationObject from 'src/@types/interfaces/api/common/balance-operation';
+
+export default interface IBalanceGetOperationsResponse extends Array<IBalanceOperationObject> {
+
+}
