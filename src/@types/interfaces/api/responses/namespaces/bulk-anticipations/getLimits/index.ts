0688bd0dc3ac596aa2d2b1eaee168286b95eb097
Douglas Alves Domingues <>
2021-08-10 15:56:30 -0300

    add response interface **IBulkAnticipationsGetLimitsResponse**

---
+++ src/@types/interfaces/api/responses/namespaces/bulk-anticipations/getLimits/index.ts
@@ -0,0 +1,4 @@
+import IBulkAnticipationLimits from 'src/@types/interfaces/api/common/bulk-anticipation/limits';
+
+export default interface IBulkAnticipationsGetLimitsResponse extends IBulkAnticipationLimits {
+}
