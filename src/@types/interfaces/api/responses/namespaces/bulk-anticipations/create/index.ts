d3fba30ad0020a31e4e34d0e230ad48461550063
Douglas Alves Domingues <>
2021-08-10 15:25:29 -0300

    add response interface **IBulkAnticipationsCreateResponse**

---
+++ src/@types/interfaces/api/responses/namespaces/bulk-anticipations/create/index.ts
@@ -0,0 +1,5 @@
+import IBulkAnticipationObject from 'src/@types/interfaces/api/common/bulk-anticipation';
+
+export default interface IBulkAnticipationsCreateResponse extends IBulkAnticipationObject {
+
+}
