2ba9185616d604a75fedc7a1f085cac41cccf7a6
Douglas Alves Domingues <>
2021-08-10 08:33:48 -0300

    update interface **IBalanceNamespaceMethods** methods with expected parameters

--- src/@types/interfaces/client/namespaces/balance/index.ts
+++ src/@types/interfaces/client/namespaces/balance/index.ts
@@ -2,9 +2,14 @@ import APIErrorResponse from 'src/@types/interfaces/api/responses/error';
 import IBalanceGetResponse from 'src/@types/interfaces/api/responses/namespaces/balance/get';
 import IBalanceGetOperationDetailResponse from 'src/@types/interfaces/api/responses/namespaces/balance/getOperationDetail';
 import IBalanceGetOperationsResponse from 'src/@types/interfaces/api/responses/namespaces/balance/getOperations';
+import IBalanceGetOperationsParameters from '../../parameters/balance/get-operations';
 
 export default interface IBalanceNamespaceMethods {
   get: () => Promise<APIErrorResponse | IBalanceGetResponse>
-  getOperations: () => Promise<APIErrorResponse | IBalanceGetOperationsResponse>
-  getOperationDetail: () => Promise<APIErrorResponse | IBalanceGetOperationDetailResponse>
+  getOperations: (
+    filters: IBalanceGetOperationsParameters,
+  ) => Promise<APIErrorResponse | IBalanceGetOperationsResponse>
+  getOperationDetail: (
+    balanceOperationId: string,
+  ) => Promise<APIErrorResponse | IBalanceGetOperationDetailResponse>
 }
