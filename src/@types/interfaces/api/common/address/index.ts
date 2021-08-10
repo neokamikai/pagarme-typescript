a10a8e10ee5a1807903d12b50afc1c44bd2243dc
Douglas Alves Domingues <>
2021-08-10 17:10:29 -0300

    add common interface **IAddress**

---
+++ src/@types/interfaces/api/common/address/index.ts
@@ -0,0 +1,9 @@
+export default interface IAddress {
+  street: string
+  complementary: string
+  street_number: string
+  neighborhood: string
+  city: string
+  state: string
+  zipcode: string
+}
