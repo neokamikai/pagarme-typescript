8b254bf977369e3dd5da149b4148099b3453ce65
Douglas Alves Domingues <>
2021-08-11 09:34:49 -0300

    update **IAddress** interface with JSDoc summary for each field

--- src/@types/interfaces/api/common/address/index.ts
+++ src/@types/interfaces/api/common/address/index.ts
@@ -1,9 +1,34 @@
 export default interface IAddress {
+  /**
+   * Obrigatório. Rua
+   */
   street: string
-  complementary: string
+  /**
+   * Obrigatório. Número
+   */
   street_number: string
-  neighborhood: string
-  city: string
-  state: string
+  /**
+   * Obrigatório. CEP. Para endereço brasileiro, deve conter uma numeração de 8 dígitos
+   */
   zipcode: string
+  /**
+   * Obrigatório. País. Duas letras minúsculas. Deve seguir o padão ISO 3166-1 alpha-2
+   */
+  country: string
+  /**
+   * Obrigatório. Estado
+   */
+  state: string
+  /**
+   * Obrigatório. Cidade
+   */
+  city: string
+  /**
+   * Bairro
+   */
+  neighborhood?: string
+  /**
+   * Complemento. Não pode ser uma string vazia nem null
+   */
+  complementary?: string
 }
