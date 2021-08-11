e4aa3c2b5c8f8cc7aa8f25b396ed6e704611374c
Douglas Alves Domingues <>
2021-08-11 16:20:06 -0300

    update interface **ITransactionOperation**: add missing fields

--- src/@types/interfaces/api/common/transaction/operation/index.ts
+++ src/@types/interfaces/api/common/transaction/operation/index.ts
@@ -1,4 +1,19 @@
-export default interface TransactionOperation<TMetadata extends Object = any> {
-
+export default interface ITransactionOperation<TMetadata extends Object = any> {
+  id: string
+  date_created: string
+  date_updated: string
+  status: string
+  fail_reason: string,
+  type: string
+  rollbacked: boolean,
+  model: string
+  model_id: string
+  group_id: string
+  next_group_id: string,
+  request_id: string
+  started_at: number,
+  ended_at: number,
+  processor: string
+  processor_response_code: string,
   metadata: TMetadata
 }
