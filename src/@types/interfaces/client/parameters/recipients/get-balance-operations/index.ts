4a072e2eae6154d63b89e7f9f6fbc2f01ead32eb
Douglas Alves Domingues <>
2021-08-12 12:24:17 -0300

    update **IRecipientsGetBalanceOperationsParameters** interface: set parameters as optional

--- src/@types/interfaces/client/parameters/recipients/get-balance-operations/index.ts
+++ src/@types/interfaces/client/parameters/recipients/get-balance-operations/index.ts
@@ -2,9 +2,9 @@ export default interface IRecipientsGetBalanceOperationsParameters {
   /**
    * Retorna n resultados, com um máximo de 1000
    */
-  count: string
+  count?: string
   /**
    * Útil para implementação de uma paginação de resultados
    */
-  page: string
+  page?: string
 }
