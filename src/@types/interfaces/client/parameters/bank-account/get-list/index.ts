b19fa80195488183eb8fe804bb72805f7eac6ba7
Douglas Alves Domingues <>
2021-08-12 09:54:48 -0300

    update parameters interface **IGetBankAccountListParameters**: set all fields as optional

--- src/@types/interfaces/client/parameters/bank-account/get-list/index.ts
+++ src/@types/interfaces/client/parameters/bank-account/get-list/index.ts
@@ -2,50 +2,50 @@ export default interface IGetBankAccountListParameters {
   /**
    * Retorna n resultados, com um máximo de 1000
    */
-  count: string
+  count?: string
   /**
    * Útil para implementação de uma paginação de resultados
    */
-  page: string
+  page?: string
   /**
    * Filtro pelo ID da conta bancária
    */
-  id: string
+  id?: string
 
   /**
    * Filtro pelo código do banco das contas bancárias
    */
-  bank_code: string
+  bank_code?: string
   /**
    * Filtro pela agencia da conta
    */
-  agencia: string
+  agencia?: string
   /**
    * Filtro pelo dígito verificador da agencia da conta
    */
-  agencia_dv: string
+  agencia_dv?: string
   /**
    * Filtro pelo número da conta
    */
-  conta: string
+  conta?: string
   /**
    * Filtro pelo dígito verificador da conta
    */
-  conta_dv: string
+  conta_dv?: string
   /**
    * Filtro pelo CPF/CNPJ atrelado à conta
    */
-  document_number: string
+  document_number?: string
   /**
    * Filtro pelo nome do detentor da conta
    */
-  legal_name: string
+  legal_name?: string
   /**
    * Filtro pela data de criação da conta
    */
-  date_created: string
+  date_created?: string
   /**
    * Alias de date_created
    */
-  created_at: string
+  created_at?: string
 }
