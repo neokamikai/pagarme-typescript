64af74efb8577d0a86319f4dfffa53691103ea1e
Douglas Alves Domingues <kamikai@gmail.com>
2022-01-21 16:15:54 -0300

    add file to centralize all response interfaces

---
+++ src/@types/interfaces/api/responses/index.ts
@@ -0,0 +1,11 @@
+import ICreateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/create';
+
+namespace Responses {
+  export namespace V5 {
+    export namespace Recipient {
+      export type Create = ICreateRecipientV5Response;
+    }
+  }
+
+}
+export default Responses;
