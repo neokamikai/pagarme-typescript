3b2e9499c9138521f847064562b02e171b6accd9
Douglas Alves Domingues <kamikai@gmail.com>
2022-01-21 15:39:05 -0300

    add **RecipientPaymentModeV5Type** type

---
+++ src/@types/enums/recipient/payment-mode/v5/index.ts
@@ -0,0 +1,9 @@
+const RecipientPaymentModeV5BankTransfer = 'bank_transfer';
+
+export const RecipientPaymentModeV5TypeEnums = {
+  RecipientPaymentModeV5BankTransfer: typeof RecipientPaymentModeV5BankTransfer,
+};
+
+type RecipientPaymentModeV5Type = typeof RecipientPaymentModeV5BankTransfer;
+
+export default RecipientPaymentModeV5Type;
