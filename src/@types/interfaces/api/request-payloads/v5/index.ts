eb2a264d47362d6f383394e6df75b665586a013d
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-08 01:06:53 -0300

    export IUpdateReceiverBankAccountPayload and IUpdateReceiverPayload

--- src/@types/interfaces/api/request-payloads/v5/index.ts
+++ src/@types/interfaces/api/request-payloads/v5/index.ts
@@ -1,3 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export { default as ICreateOrderPayload } from './order/create';
 export { default as ICreateReceiverPayload } from './recipient/create';
+export { default as IUpdateReceiverBankAccountPayload } from './recipient/update/bank-account';
+export { default as IUpdateReceiverPayload } from './recipient/update';
