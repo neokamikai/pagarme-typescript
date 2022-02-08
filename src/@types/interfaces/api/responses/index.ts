5ae662bbd44d02af79846c6f510f2aadbc0fefc0
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-08 01:05:38 -0300

    export **Responses.V5.Recipient.Update**

--- src/@types/interfaces/api/responses/index.ts
+++ src/@types/interfaces/api/responses/index.ts
@@ -1,9 +1,11 @@
 import ICreateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/create';
+import IUpdateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/update';
 
 namespace Responses {
   export namespace V5 {
     export namespace Recipient {
       export type Create = ICreateRecipientV5Response;
+      export type Update = IUpdateRecipientV5Response;
     }
   }
 
