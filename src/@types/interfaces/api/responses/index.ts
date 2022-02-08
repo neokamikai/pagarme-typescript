8466a66a9cef88ecba05b014635508050a38de7b
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-08 01:32:58 -0300

    export TransferSettings

--- src/@types/interfaces/api/responses/index.ts
+++ src/@types/interfaces/api/responses/index.ts
@@ -1,6 +1,7 @@
 import ICreateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/create';
 import IUpdateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/update';
 import IBalanceRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/balance';
+import ITransferSettingsRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/transfer/settings';
 
 namespace Responses {
   export namespace V5 {
@@ -8,6 +9,7 @@ namespace Responses {
       export type Create = ICreateRecipientV5Response;
       export type Update = IUpdateRecipientV5Response;
       export type Balance = IBalanceRecipientV5Response;
+      export type TransferSettings = ITransferSettingsRecipientV5Response;
     }
   }
 
