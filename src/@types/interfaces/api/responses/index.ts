ea351600f2edd4884bd911eefcece1910582aa2a
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-08 01:21:08 -0300

    export **Responses.V5.Recipient.Balance**

--- src/@types/interfaces/api/responses/index.ts
+++ src/@types/interfaces/api/responses/index.ts
@@ -1,11 +1,13 @@
 import ICreateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/create';
 import IUpdateRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/update';
+import IBalanceRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/balance';
 
 namespace Responses {
   export namespace V5 {
     export namespace Recipient {
       export type Create = ICreateRecipientV5Response;
       export type Update = IUpdateRecipientV5Response;
+      export type Balance = IBalanceRecipientV5Response;
     }
   }
 
