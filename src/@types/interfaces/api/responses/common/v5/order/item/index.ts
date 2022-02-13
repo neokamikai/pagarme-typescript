5d3f100054cacc969694c6b7f88809d8cede0ed4
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-13 00:05:07 -0300

    add **OrderV5Item** interface

---
+++ src/@types/interfaces/api/responses/common/v5/order/item/index.ts
@@ -0,0 +1,11 @@
+export default interface OrderV5Item {
+  id: string
+  type: string
+  description: string
+  amount: number
+  quantity: number
+  status: string
+  created_at: string
+  updated_at: string
+  code: string
+}
