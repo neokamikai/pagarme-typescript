a948cb32caeee808909c13b36e90c0fecb14a7f7
Douglas Alves Domingues <kamikai@gmail.com>
2022-02-19 00:12:45 -0300

    add **ICreateStandaloneSubscriptionV5Response** interface

--- src/@types/interfaces/api/responses/index.ts
+++ src/@types/interfaces/api/responses/index.ts
@@ -4,6 +4,7 @@ import IBalanceRecipientV5Response from 'src/@types/interfaces/api/responses/nam
 import ITransferSettingsRecipientV5Response from 'src/@types/interfaces/api/responses/namespaces/recipients/v5/transfer/settings';
 import IListRecipientV5Response from './namespaces/recipients/v5/list';
 import IRecipientV5 from '../common/recipient/v5';
+import ICreateStandaloneSubscriptionV5Response from './namespaces/subscription/create';
 
 namespace Responses {
   export namespace V5 {
@@ -14,6 +15,11 @@ namespace Responses {
       export type TransferSettings = ITransferSettingsRecipientV5Response;
       export type List = IListRecipientV5Response;
       export type Detail = IRecipientV5;
+    }
+    export namespace Subscription {
+      export namespace Standalone {
+        export type Create = ICreateStandaloneSubscriptionV5Response;
+      }
     }
   }
 
---
+++ src/@types/interfaces/api/responses/namespaces/subscription/create/index.ts
@@ -0,0 +1,5 @@
+import StandaloneSubscription from '../../../common/v5/subscription/standalone';
+
+export default interface ICreateStandaloneSubscriptionV5Response extends StandaloneSubscription {
+
+}
